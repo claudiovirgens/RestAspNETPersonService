4a293964fa9dbf0c5d3421d66f0e25626019161b
Cláudio Santos das Virgens <claudiosantosvirgens@outlook.com>
2018-09-14 17:25:50 -0300

    Database Imtegration

--- RestAspNETPersonService/Startup.cs
+++ RestAspNETPersonService/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestAspNETPersonService.Context;
 using RestAspNETPersonService.Services;
 using RestAspNETPersonService.Services.Implementations;
 
@@ -27,6 +29,10 @@ namespace RestAspNETPersonService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             //Dependency Injection  *********************************
---
+++ RestAspNETPersonService/db/Script_Person_RestAspNetUdemy.sql
@@ -0,0 +1,12 @@
+/*
+
+use restaspnetdb;
+
+create table persons (
+Id int(10)Unsigned null default null,
+FirstName VARCHAR(50) NULL DEFAULT NULL,
+LastName VARCHAR(50) NULL DEFAULT NULL,
+Address VARCHAR(50) NULL DEFAULT NULL,
+Gender VARCHAR(50) NULL DEFAULT NULL)
+Engine=InnoDB;
+*/
