5a0bd486dda18bde4a05780638ef2cd1c6242d97
Cláudio Santos das Virgens <claudiosantosvirgens@outlook.com>
2018-09-16 20:28:29 -0300

    Refactoring the update method

--- RestAspNETPersonService/Repository/Implementations/PersonRepositoryImpl.cs
+++ RestAspNETPersonService/Repository/Implementations/PersonRepositoryImpl.cs
@@ -59,7 +59,7 @@ namespace RestAspNETPersonService.Repository.Implementations
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) return new Person();
+            if (!Exists(person.Id)) return null;
             
             var result = _context.Persons.SingleOrDefault(b => b.Id == person.Id);
             if (result != null) { 
